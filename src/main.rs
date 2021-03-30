301999d7c00ad53ac52eac8569a9c5c5918b43ea
Michał Sala <0havaker@gmail.com>
2021-03-31 00:23:31 +0200

    Elf representation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use goblin::{
-    elf::{self, Elf},
-    error,
-    strtab::Strtab,
-    Object,
-};
-use std::collections::HashMap;
-use std::ffi::CString;
+use goblin::elf::section_header::*;
+use goblin::{elf, error, strtab::Strtab, Object};
 use std::fs;
 use std::{collections::BTreeMap, rc::Rc};
 
@@ -20,19 +14,13 @@ fn main() -> error::Result<()> {
     Ok(())
 }
 
-use goblin::elf::sym::*;
-use goblin::{
-    container::{Container, Ctx, Endian},
-    elf::{header::*, section_header::*},
-};
-use scroll::Pwrite;
-use std::io::Write;
-
 #[derive(Debug, Clone)]
 struct Section {
-    header: SectionHeader,
-    name: String,
+    header: elf::SectionHeader,
     content: Vec<u8>,
+
+    // An object file can have more than one section with the same name.
+    name: String,
 }
 
 impl Section {
@@ -54,22 +42,53 @@ impl Section {
 
 #[derive(Debug, Clone)]
 struct Symbol {
-    name: String,
+    name: Option<String>,
     section: Option<Rc<Section>>,
-    sym: Sym,
+    sym: elf::Sym,
 }
 
-// An object file can have more than one section with the same name.
 #[derive(Debug, Clone)]
-struct Sections {
-    sections: BTreeMap<usize, Rc<Section>>,
-    symbols: Vec<Symbol>,
+struct Reloc {
+    reloc: elf::Reloc,
+    symbol: Rc<Symbol>,
 }
 
-impl Sections {
-    fn new(elf: &Elf, bytes: &Vec<u8>) -> Sections {
+#[derive(Debug, Clone)]
+struct RelocSection {
+    target: Rc<Section>,
+    relocs: Vec<Reloc>,
+}
+
+impl RelocSection {
+    fn new(
+        target: Rc<Section>,
+        reloc_section: &elf::RelocSection,
+        symbols: &Vec<Rc<Symbol>>,
+    ) -> Self {
+        let mut relocs = Vec::with_capacity(reloc_section.len());
+
+        for reloc in reloc_section.iter() {
+            let symbol = symbols.get(reloc.r_sym).expect("").clone();
+            let r = Reloc { reloc, symbol };
+            relocs.push(r);
+        }
+
+        RelocSection { target, relocs }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Elf {
+    sections: BTreeMap<usize, Rc<Section>>,
+    symbols: Vec<Rc<Symbol>>,
+    reloc_sections: Vec<RelocSection>,
+}
+
+impl Elf {
+    fn new(elf: &elf::Elf, bytes: &Vec<u8>) -> Elf {
         let mut sections = BTreeMap::new();
         let mut symbols = Vec::new();
+        let mut reloc_sections = Vec::new();
 
         for (ix, header) in elf.section_headers.iter().enumerate() {
             let section = Section::extract(header, &elf.shdr_strtab, bytes);
@@ -78,26 +97,47 @@ impl Sections {
 
         for sym in elf.syms.iter() {
             let name = match sym.st_name {
-                0 => String::new(),
-                _ => elf
-                    .strtab
-                    .get(sym.st_name)
-                    .expect("invalid shstrtab index")
-                    .expect("invalid section name")
-                    .to_string(),
+                0 => None,
+                _ => Some(
+                    elf.strtab
+                        .get(sym.st_name)
+                        .expect("invalid shstrtab index")
+                        .expect("invalid section name")
+                        .to_string(),
+                ),
             };
 
             let section = sections.get(&sym.st_shndx).map(|rc| rc.clone());
 
             let symbol = Symbol { name, section, sym };
-            symbols.push(symbol);
+            symbols.push(Rc::new(symbol));
         }
 
-        Sections { sections, symbols }
+        for (section_ix, relocs) in elf.shdr_relocs.iter() {
+            let section = sections
+                .get(section_ix)
+                .map(|rc| rc.clone())
+                .expect("invalid section index");
+
+            let reloc_section = RelocSection::new(section, relocs.clone(), &symbols);
+            reloc_sections.push(reloc_section);
+        }
+
+        Elf {
+            sections,
+            symbols,
+            reloc_sections,
+        }
+    }
+
+    fn serialize(&self) -> Vec<u8> {
+        let mut buf = Vec::new();
+
+        buf
     }
 }
 
-fn process(elf: Elf, buffer: &Vec<u8>) {
-    let sections = Sections::new(&elf, buffer);
-    dbg!(&sections.symbols);
+fn process(elf: elf::Elf, buffer: &Vec<u8>) {
+    let sections = Elf::new(&elf, buffer);
+    dbg!(&sections.reloc_sections);
 }
